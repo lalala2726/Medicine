d0ea82465a282e7a96f2b8092f20c9ea148d8223
Chuang <Chuang003@outlook.com>
2025-08-27 19:15:32 +0800

    Initial commit

---
+++ src/test/java/cn/zhangchuangla/medicine/MedicineApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zhangchuangla.medicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
