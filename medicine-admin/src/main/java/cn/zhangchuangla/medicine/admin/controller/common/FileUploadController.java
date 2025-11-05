5f9493216493cb7534e2859e56f89a43bf8a5376
Chuang <Chuang003@outlook.com>
2025-11-05 08:51:01 +0800

    feat(file): 添加文件上传接口的Swagger注解

--- medicine-admin/src/main/java/cn/zhangchuangla/medicine/admin/controller/common/FileUploadController.java
+++ medicine-admin/src/main/java/cn/zhangchuangla/medicine/admin/controller/common/FileUploadController.java
@@ -5,6 +5,7 @@ import cn.zhangchuangla.medicine.common.core.base.AjaxResult;
 import cn.zhangchuangla.medicine.common.security.base.BaseController;
 import cn.zhangchuangla.medicine.model.vo.FileUploadVo;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @RestController
 @RequestMapping("/file")
+@Tag(name = "文件上传接口", description = "文件上传接口")
 public class FileUploadController extends BaseController {
 
     private final FileUploadService fileUploadService;
