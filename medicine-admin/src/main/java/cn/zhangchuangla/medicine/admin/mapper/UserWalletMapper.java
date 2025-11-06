99ecd84cd7e7f3a40b270093aaae6044d276fc38
Chuang <Chuang003@outlook.com>
2025-11-06 08:29:42 +0800

    feat(admin): 新增用户钱包及钱包日志功能模块

---
+++ medicine-admin/src/main/java/cn/zhangchuangla/medicine/admin/mapper/UserWalletMapper.java
@@ -0,0 +1,15 @@
+package cn.zhangchuangla.medicine.admin.mapper;
+
+import cn.zhangchuangla.medicine.model.entity.UserWallet;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Chuang
+*/
+public interface UserWalletMapper extends BaseMapper<UserWallet> {
+
+}
+
+
+
+
---
+++ medicine-admin/src/main/java/cn/zhangchuangla/medicine/admin/service/impl/UserWalletServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.zhangchuangla.medicine.admin.service.impl;
+
+import cn.zhangchuangla.medicine.admin.mapper.UserWalletMapper;
+import cn.zhangchuangla.medicine.admin.service.UserWalletService;
+import cn.zhangchuangla.medicine.model.entity.UserWallet;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+* @author Chuang
+*/
+@Service
+public class UserWalletServiceImpl extends ServiceImpl<UserWalletMapper, UserWallet>
+    implements UserWalletService{
+
+}
+
+
+
+
