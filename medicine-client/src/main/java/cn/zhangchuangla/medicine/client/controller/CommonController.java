d6c1509b7d6147badc2e9c1b397da21c0260a456
Chuang <Chuang003@outlook.com>
2025-11-20 21:33:52 +0800

    feat(common): 添加文件上传接口并更新MinIO配置
    - 新增CommonController，实现文件上传功能接口
    - 集成FileUploadService进行文件存储处理
    - 使用Swagger注解完善接口文档
    - 修改application.yml，更新MinIO连接配置和访问密钥
    - 设置上传接口路径为/common/upload，支持MultipartFile参数

---
+++ medicine-client/src/main/java/cn/zhangchuangla/medicine/client/controller/CommonController.java
@@ -0,0 +1,43 @@
+package cn.zhangchuangla.medicine.client.controller;
+
+import cn.zhangchuangla.medicine.admin.common.storage.service.FileUploadService;
+import cn.zhangchuangla.medicine.common.core.base.AjaxResult;
+import cn.zhangchuangla.medicine.common.security.base.BaseController;
+import cn.zhangchuangla.medicine.model.vo.FileUploadVo;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author Chuang
+ * <p>
+ * created on 2025/11/20
+ */
+@RestController
+@RequestMapping("/common")
+@Slf4j
+public class CommonController extends BaseController {
+
+    private final FileUploadService fileUploadService;
+
+    public CommonController(FileUploadService fileUploadService) {
+        this.fileUploadService = fileUploadService;
+    }
+
+
+    /**
+     * 文件上传接口
+     *
+     * @param file 文件
+     * @return 文件上传结果
+     */
+    @PostMapping("/upload")
+    @Operation(summary = "文件上传")
+    public AjaxResult<FileUploadVo> upload(MultipartFile file) {
+        FileUploadVo upload = fileUploadService.upload(file);
+        return success(upload);
+    }
+}
