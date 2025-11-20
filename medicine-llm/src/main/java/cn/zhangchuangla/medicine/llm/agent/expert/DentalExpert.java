6021e9797dc0c6e816e24873930f1695a15e4a31
Chuang <Chuang003@outlook.com>
2025-11-20 23:41:19 +0800

    feat(agent): 新增多科室专家节点类
    - 添加口腔科专家节点类 DentalExpert
    - 添加皮肤科专家节点类 DermatologyExpert
    - 添加耳鼻喉科专家节点类 EntExpert
    - 添加眼科专家节点类 EyeSpecialist
    - 添加妇科专家节点类 GynecologyExpert
    - 添加普通内科专家节点类 InternalMedicineExpert
    - 添加儿科专家节点类 PediatricsExpert
    - 添加心理与精神科专家节点类 PsychologyExpert
    - 添加外科与骨科专家节点类 SurgeryAndOrthopedicsExpert

---
+++ medicine-llm/src/main/java/cn/zhangchuangla/medicine/llm/agent/expert/DentalExpert.java
@@ -0,0 +1,11 @@
+package cn.zhangchuangla.medicine.llm.agent.expert;
+
+/**
+ * 口腔科专家节点
+ *
+ * @author Chuang
+ * <p>
+ * created on 2025/11/20
+ */
+public class DentalExpert {
+}
---
+++ medicine-llm/src/main/java/cn/zhangchuangla/medicine/llm/agent/expert/PediatricsExpert.java
@@ -0,0 +1,11 @@
+package cn.zhangchuangla.medicine.llm.agent.expert;
+
+/**
+ * 儿科专家节点
+ *
+ * @author Chuang
+ * <p>
+ * created on 2025/11/20
+ */
+public class PediatricsExpert {
+}
