b99baf3ba10831b08d004cea5c890d5a2fce23ea
Chuang <Chuang003@outlook.com>
2025-12-01 08:42:30 +0800

    refactor(model): 优化Card接口及导入声明
    - 在Card接口中添加获取卡类型的方法getCardType()
    - 精简ClientConsultationTools的导入语句，使用通配符导入相关响应类
    - 删除了对具体响应类的单独导入，提高代码整洁度

--- medicine-llm/src/main/java/cn/zhangchuangla/medicine/llm/model/response/Card.java
+++ medicine-llm/src/main/java/cn/zhangchuangla/medicine/llm/model/response/Card.java
@@ -1,9 +1,14 @@
 package cn.zhangchuangla.medicine.llm.model.response;
 
+import cn.zhangchuangla.medicine.llm.model.enums.CardType;
+
 /**
  * @author Chuang
  * <p>
  * created on 2025/11/30
  */
 public interface Card {
+
+    CardType getCardType();
+
 }
